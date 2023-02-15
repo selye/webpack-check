8ba8378633061483016d03fb9bb32baadd4e36ec
shijie <1193033232@qq.com>
2023-02-15 23:26:50 +0800

    feat: webpack初始化代码

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./main.css";
+
+const a = "hello Item";
+console.log(a);
+module.exports = a;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+console.log("process.env.NODE_ENV=", process.env.NODE_ENV);
+const config = {
+  mode: "development", // 模式
+  entry: "./src/index.js", // 打包入口地址
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    compress: true,
+    port: 8080,
+  },
+  output: {
+    filename: "bundle.js", // 输出文件名
+    path: path.join(__dirname, "dist"), // 输出文件目录
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = (env, arg) => {
+  console.log("arg.mode=", arg.mode);
+  return config;
+};
